e3b64f19062e1332618ad102c2781f9b0aa6f15e
VALADIER Thomas <1.thomas.valadier@orange.fr>
2016-10-12 03:28:20 +0200

    .

--- controllers/ajoutquizz_controller.php
+++ controllers/ajoutquizz_controller.php
@@ -12,13 +12,14 @@ require '../views/ajoutquizz.php';
 if (isset($_POST['send'])) {
     if (!empty($_POST['categorie']) && !empty($_POST['titre'])) {
         require_once '../models/quizz.class.php';
-        $categorie = strtolower(htmlspecialchars(($_POST['categorie'])));
+        $categorie = htmlspecialchars(($_POST['categorie']));
         $title = htmlspecialchars(addslashes($_POST['titre']));
         $add = new quizz($title, $categorie);
         $add->addQuizz();
         $resu = $add->seeID();
         $data = $resu->id_quizz;
-        header('location:accueil.php?quizz=' . $data);
+        ?>
+        <script>document.location = 'accueil.php?quizz='+ <?php echo $data ?></script> <?php
     } else {
         ?>
         <script>alert("rempli tous les champs batard")</script><?php
--- views/ajoutquizz.php
+++ views/ajoutquizz.php
@@ -1,15 +1,18 @@
-<h2>Ajouter un quizz</h2>
+<h2 style="text-align: center">Ajouter un quizz</h2><br><br>
 
 <form action="" method="post">
-    <input type="text" name="titre" id="titre" placeholder="Titre"><br>
-
-    <select name="categorie" id="">
-        <option value="" disabled selected>Choix catégorie</option>
+    <div class="form-group col-md-offset-3 col-md-6">
+    <input class="form-control" type="text" name="titre" id="titre" placeholder="Titre">
+    </div>
+    <div class="form-group col-md-offset-3 col-md-6">
+    <select class="form-control"  name="categorie" id="">
+        <option class="form-control" value="" disabled selected>Choix catégorie</option>
         <?php
         foreach ($result as $data): ?>
             <option value="<?= $data->categorie ?>"><?= $data->categorie ?></option>
         <?php endforeach; ?>
-    </select><br>
+    </select>
+    </div><br>
     <br>
-    <button name="send" class="btn btn-default" type="submit">Commencer</button>
+    <button name="send" class="btn btn-success col-md-4 col-md-offset-4" type="submit">Créer</button>
 </form>
